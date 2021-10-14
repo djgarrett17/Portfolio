c2ef6c417889916288cee1c0ea71f994d4b6051e
Devin Garrett <devingarrett@Devins-MacBook-Pro.local>
2021-10-13 21:35:41 -0500

    mailchimp add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom'
 import './styles/Home.css';
 import $ from 'jquery';
 
+import Mailchimp from 'react-mailchimp-form'
 // import "react-responsive-carousel/lib/styles/carousel.min.css";
 // import { Carousel } from 'react-responsive-carousel';
 import MyCarousel from '../components/carousel/Carousel.js'
 import MyCarouselMobile from '../components/carousel/CarouselMobile.js'
+
+import { BsArrowRightShort } from "react-icons/bs";
 
 const Home = () => {
 
@@ -26,6 +29,32 @@ const Home = () => {
                 <div className="carouselMobile">
                     <MyCarouselMobile/>
                 </div>
+            </div>
+            <div>
+                <p className="subscribeTxt">Subscribe to my newsletter to receive your prize!</p>
+            </div>
+            <div className="emailDiv">
+            <Mailchimp 
+                action="https://kingpong.us7.list-manage.com/subscribe/post?u=bdd1fa2695abb33207f964b14&amp;id=ed81bdc154"
+                fields={[
+                  {
+                    name: 'EMAIL',
+                    placeholder: 'Enter your email',
+                    type: 'email',
+                    required: true
+                  }
+                ]}
+                messages = {
+                    {
+                      sending: "Sending...",
+                      success: "Thank you for subscribing!",
+                      error: "An unexpected internal error has occurred.",
+                      empty: "Please enter valid email.",
+                      duplicate: "Too many subscribe attempts for this email address",
+                      button: <BsArrowRightShort size={"32px"} />
+                    }
+                  }
+                />
             </div>
         </div>  
         
